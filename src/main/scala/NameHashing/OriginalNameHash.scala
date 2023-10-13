e46c81356029abe27abbc0aed35ed9b14c79627b
rskorina <icestar.ru@gmail.com>
2023-10-13 12:02:45 -0400

    original name hash setup

---
+++ src/main/scala/NameHashing/OriginalNameHash.scala
@@ -0,0 +1,71 @@
+package NameHashing
+
+class OriginalNameHash extends NameHash{
+    val firstNameDictionary = Map(
+        "a" -> "",
+        "b" -> "",
+        "c" -> "",
+        "d" -> "",
+        "e" -> "",
+        "f" -> "",
+        "g" -> "",
+        "h" -> "",
+        "i" -> "",
+        "j" -> "",
+        "k" -> "",
+        "l" -> "",
+        "m" -> "",
+        "n" -> "",
+        "o" -> "",
+        "p" -> "",
+        "q" -> "",
+        "r" -> "",
+        "s" -> "",
+        "t" -> "",
+        "u" -> "",
+        "v" -> "",
+        "w" -> "",
+        "x" -> "",
+        "y" -> "",
+        "z" -> "",
+    )
+
+    val lastNameDictionary = Map(
+        "a" -> "",
+        "b" -> "",
+        "c" -> "",
+        "d" -> "",
+        "e" -> "",
+        "f" -> "",
+        "g" -> "",
+        "h" -> "",
+        "i" -> "",
+        "j" -> "",
+        "k" -> "",
+        "l" -> "",
+        "m" -> "",
+        "n" -> "",
+        "o" -> "",
+        "p" -> "",
+        "q" -> "",
+        "r" -> "",
+        "s" -> "",
+        "t" -> "",
+        "u" -> "",
+        "v" -> "",
+        "w" -> "",
+        "x" -> "",
+        "y" -> "",
+        "z" -> "",
+    )
+  }
+  	tangle	wild	swift	rain	thorn	fuzzy	mud	loud	bramble	moss	leopard	scar	bright	running	sand	dawn	ice	spotted	rock	red	mouse	tiger	flower	claw	blue
+  
+  def hash(firstName: String, lastName: String): String = {
+
+  }
+
+  def HashType(): Unit = {
+    //
+  }
+}
